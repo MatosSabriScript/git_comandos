bc33303e55af677c365d33a12f0294772a0fce2c
Sabrina Matos <sabrinamatosalmeida2@gmail.com>
2024-03-14 20:54:46 -0300

    arrumando

--- stle.js
+++ stle.js
@@ -4,3 +4,19 @@ const teste= ()=>{
 
 let tese= teste()
 console.log(tese)
+
+
+
+
+function checkWord() {
+    try {
+        var input = document.getElementById('inputWord').value.toLowerCase();
+        if (input === "banana") {
+            throw 'Palavra liberada: ';
+        } else {
+            document.getElementById('message').textContent = 'Palavra negada: ' + input;
+        }
+    } catch (error) {
+        document.getElementById('message').textContent = error;
+    }
+}
